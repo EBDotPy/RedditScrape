347ba3b1d8e1980b6b508b2ff4d1dfc1887873b3
homer2020 <ericmbaird@gmail.com>
2023-01-24 21:59:52 -0500

    added schedule

--- redditsSDscrape.py
+++ redditsSDscrape.py
@@ -2,29 +2,43 @@
 
 import discord
 from discord.ext import commands
-from cred import client_id, client_secret, username, password, user_agent, token
-# from credentials import client_id, client_secret, username, password, user_agent, token
+import schedule
+import time
 import praw
 import datetime
+from cred import client_id, client_secret, username, password, user_agent, token
+# from credentials import client_id, client_secret, username, password, user_agent, token
 
-intents = discord.Intents().all()
-bot = commands.Bot(command_prefix='!', intents=intents)
-now = datetime.datetime.now()
+bot = commands.Bot(command_prefix='!')
 
 @bot.command()
 async def top_posts(ctx):
-    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
-                     username=username, password=password, user_agent=user_agent)
-    posts = reddit.subreddit('StableDiffusion').new(limit=1000)
-    count = 1
-    output = ""
-    for post in posts:
-        if post.link_flair_text == "Workflow Included" and post.created_utc > (now - datetime.timedelta(days=1)).timestamp():
-            output += str(count) + ": " + post.title + "\n"
-            output += post.permalink + "\n"
-            count += 1
-            if count > 5:
-                break
-    await ctx.send(output)
+    try:
+        reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
+                             username=username, password=password, user_agent=user_agent)
+        posts = reddit.subreddit('StableDiffusion').new(limit=1000)
+        now = datetime.datetime.now()
+        count = 1
+        output = ""
+        for post in posts:
+            if post.link_flair_text == "Workflow Included" and post.created_utc > (now - datetime.timedelta(days=1)).timestamp():
+                output += str(count) + ": " + post.title + "\n"
+                output += post.url + "\n"
+                count += 1
+                if count > 5:
+                    break
+        await ctx.send(output)
+    except Exception as e:
+        await ctx.send("An error occured. Please make sure that the subreddit exist or try again later.")
+
+def job():
+    bot.loop.create_task(bot.get_command('top_posts').callback(None, None))
+
+schedule.every().day.at("08:00").do(job)
 
 bot.run(token)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
