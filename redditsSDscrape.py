c1c29aad08d3424f1a40cc1a7a335d1528557abc
homer2020 <ericmbaird@gmail.com>
2023-01-24 21:52:06 -0500

    Discord Botified

--- redditsSDscrape.py
+++ redditsSDscrape.py
@@ -1,30 +1,30 @@
 # Simple program to get some posts and URL form the SD reddit.
 
+import discord
+from discord.ext import commands
+from cred import client_id, client_secret, username, password, user_agent, token
+# from credentials import client_id, client_secret, username, password, user_agent, token
 import praw
 import datetime
-from cred import client_id, client_secret, username, password, user_agent
-# from credentials import client_id, client_secret, username, password, user_agent
 
-# Create a Reddit instance
-reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
-                     username=username, password=password, user_agent=user_agent)
-
-
-# Get the current time
+intents = discord.Intents().all()
+bot = commands.Bot(command_prefix='!', intents=intents)
 now = datetime.datetime.now()
 
-# File name format
-file_name = "StableDiffusion_top_posts_{}.txt".format(now.strftime("%Y-%m-%d"))
-
-# Open the file for writing
-with open(file_name, "w") as f:
-    # Get the top 5 posts from the last 24 hours with the flair "Workflow Included" from the "StableDiffusion" subreddit
+@bot.command()
+async def top_posts(ctx):
+    reddit = praw.Reddit(client_id=client_id, client_secret=client_secret,
+                     username=username, password=password, user_agent=user_agent)
     posts = reddit.subreddit('StableDiffusion').new(limit=1000)
     count = 1
+    output = ""
     for post in posts:
         if post.link_flair_text == "Workflow Included" and post.created_utc > (now - datetime.timedelta(days=1)).timestamp():
-            f.write(str(count) + ": " + post.title + "\n")
-            f.write(post.permalink + "\n")
+            output += str(count) + ": " + post.title + "\n"
+            output += post.permalink + "\n"
             count += 1
             if count > 5:
                 break
+    await ctx.send(output)
+
+bot.run(token)
